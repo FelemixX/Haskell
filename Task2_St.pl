f11e3f5a6deb70ce8e04aa83ade40698b03f0a77
FelemixX <83292474+FelemixX@users.noreply.github.com>
2023-02-10 00:41:28 +0300

    Update Task2_St.pl

--- Task2_St.pl
+++ Task2_St.pl
@@ -1,49 +1,109 @@
 %2.2
-auto(moskvitch, 9500, green, 1).
-auto(moskvitch, 3000, blue, 5).
-auto(volga, 15000, black, 1).
-auto(volga, 8000, white, 6).
-auto('UAZ', 9000, green, 3).
-auto('VAZ', 6000, white, 4).
-auto('VAZ', 4000, blue, 10).
-auto('VAZ-2108', 8000, grey, 2)
-%b)
-auto(Brand,Cost,Color,Age), Cost < 5000.
-%c)
-Buy(Brand,10000).
-%d)
-car(Car, Cost, Color, Age), Buy(Автомобиль,10000).
-%2.3
-car(brand('VAZ',2108), color(blue), price($(7500)), Age).
+auto('audi', 10000, black, 1).
+auto('toyota', 15000, red, 10).
+auto('yoba', 5000, orange, 0).
+auto('junk', 1000, rust, 100500).
+auto('UAZ', 8000, green, 25).
+auto('UAZ', 12000, cyan, 15).
+auto('ford', 9000, blue, 2).
+auto('mazda', 25000, black, 10).
+auto('Cherry Volga', 123123, cherry, 50).
 
-has(student('Oleg'), book('Griboyedov','Woe from Wit')).
+
+/* b. car(Brand, Cost, Color, Age), Cost < 5000.
+	c. buy(Brand, Cost, Color, Age) :-	auto(Brand, Cost, blue, Age), Age < 3 ; auto(Brand, Cost, green, Age), Age < 3. 
+			buy(Brand, Cost, Color, Age).
+	d.  auto(Brand, 10000, Color, Age).
+*/
+buy(Brand, Cost, Color, Age) :-	auto(Brand, Cost, blue, Age), Age < 3, Cost  ; auto(Brand, Cost, green, Age), Age < 3. 
+
 %2.4
-%b)
+has('Oleg', book('Pushkin', 'Captains Daughter')).
+has('Lena', book('Monten', 'Expirements')).
+has('Ira', handkerchief(blue)).
+has('Lena', handkerchief(red)).
+
+/* 
 has(Who, book('Monten', Book)).
-has(Кто, книга(Author, Book)).
-has(Кто, Что).
-has('Лена', 'Синий платок').
-op(550,xfx,имеет).
-Whoo has book('Monten', Book).
-Who has book(Author, Book).
-Who has What.
-'Lena' has 'Blue handkerchief'.
+has(Who, book(Name)). 
+has(Who, What).
+has('Lena', handkerchief(red)). %false
+*/
+
 %2.5
-lives(Creature, Environment), not(lives(Creature, Environment2)), not(lives(Creature, Environment3)), Environment \= Environment2, Environment2 \= Environment3.
-lives(Creature, Environment), aggregate_all(count, живёт(Creature,  _), Count), Count = 1.
-lives(Creature, Environment),lives(Creature,AnotherEnvironment),Environment \= AnotherEnvironment.
+
+lives('Zebra', ground).
+lives('Dog', ground).
+lives('Carp', water).
+lives('Whale', water).
+lives('Cat', Where) :- lives('Dog', Where).
+lives('Croco' , water).
+lives('Croco', ground).
+lives('Frog', water).
+lives('Frog', ground).
+lives('Duck', water).
+lives('Duck', ground).
+lives('Duck', air).
+lives('Eagle', ground).
+lives('Eagle', air).
+lives('Thunderbird', water).
+lives('Thunderbird', air).
+lives('Troubles', ass).
+
+/* 
+a.lives(Creature, Env),lives(Creature,Env2),Env\=Env2. %???
+b. lives(Creature,Env), aggregate_all(count, lives(Creature,  _), Count), Count = 1.
+*/
+
 %2.6
-%b)
-'Pyotr' has What.
-Who has rub(Money), Money >= 10000.
-Who has What.
-'Ivan' has Item, not('Nikolay' has Item), Item \= rub(Money).
-%d)
-'Pyotr' has rub(Money), cost(video, rub(Cost)), Money >= Cost.
-has(MostRich, rub(BiggetSum)), has(Who2, rub(Sum2)), has(Who3, rub(Sum3)), BiggestSum > Sum2, Sum2 > Sum3, MostRich \= Who2, MostRich \= Who3, Who2 \= Who3.
-%f)
-buy('Kolya', What).
-buy('Kolya', Item), 'Ivan' has Item.
-%i)
-'Mary' has car(Brand).
-buy('Anna', Item), not('Mary' has Item).
+%c
+price('Cherry Volga', rubles(32000)).
+price(television, rubles(8400)).
+price(freezer, rubles(4200)).
+price('Cassete player', rubles(35000)).
+price(video, rubles(12000)).
+price(reciever, rubles(1300)).
+price(clock, rubles(500)).
+
+%a
+has('Ivan', rubles(10000)).
+has('Ivan', television).
+has('Ivan', auto('Cherry Volga',123123,cherry,50)).
+has('Ivan', 'Cassete player').
+has('Pyotr', rubles(5000)).
+has('Pyotr', television).
+has('Pyotr', freezer).
+has('Nick', rubles(20000)).
+has('Nick', television).
+
+/*
+%b
+1.has('Pyotr', What).
+2.has(Who, rubles(10000)).
+3.has(Who, What).
+4.has('Ivan', Item), not('Nick', Item).
+
+%d
+has('Pyotr', rubles(Money)), price(video, rubles(Cost)), Money >= Cost.
+has(Rich, rubles(TheMost)), has(Who, rubles(Sum)), has(Who2, rubles(Sum2)), has(Who3, rubles(Sum3)), TheMost>Sum,Sum>Sum2,Sum2>Sum3,rich\=Who2,Rich\=Who3,Who2\=Who3. %???
+
+%f 
+has('Nick', rubles(Money)), price(Item, rubles(Cost)), Money >= Cost.
+has('Nick', Item), has('Ivan', Item), Item \= rubles(Money).
+*/
+
+%g
+married('Ann', 'Kolya').
+married('Mary', 'Ivan').
+married('Olga', 'Pyotr').
+
+/*has(married(Wife, Husband), Item) :- has(Husband, Item).
+*/
+%h
+%bothHave(Who, What) := has(Who, Item), Who = married(_, Husband). %???
+
+/* 
+%i
+has('Mary', auto(Brand, Cost, Color, Age).
+has('Ann', Item), not(has('Mary', Item2)), Item < rubles(Money), Item \= Item2.
+*/
